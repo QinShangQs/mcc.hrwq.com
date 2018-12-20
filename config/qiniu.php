9741a1bc09f945953b29846de99ea6191301652a
liziqiang <liziqiang.qs@outlook.com>
2018-12-20 22:25:02 +0800

    fixed

--- config/qiniu.php
+++ config/qiniu.php
@@ -7,7 +7,7 @@ return [
     'PIPELINE'  => 'hrwq20160811',
     'FREE_TIME'  => '600',
     'COVER_TIME'  => '30',
-    'DOMAIN' => 'http://oflmtu502.bkt.clouddn.com/',
+    'DOMAIN' => 'http://video.hrwq.com/',
     'AK'  => 'CNSLO6Zft_HXWPuVve4fvH98kMbfIdlJBJaAlJCA',
     'SK'  => '1mG8GK61QS8TRosOhAQVa1IVnSysvgX1X07Ed32N',
 ];
