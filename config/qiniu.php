b67cbabb5caec03c4c662f6402100ac3addd7abf
liziqiang <liziqiang.qs@outlook.com>
2019-03-11 15:31:51 +0800

    fixed

--- config/qiniu.php
+++ config/qiniu.php
@@ -5,7 +5,7 @@ return [
     'BUCKET_NAME_FILE'  => 'hrwq-video',
     'BUCKET_NAME_AUDIO'  => 'hrwq-video',
     'PIPELINE'  => 'hrwq20160811',
-    'FREE_TIME'  => '600',
+    'FREE_TIME'  => '300',
     'COVER_TIME'  => '30',
     'DOMAIN' => 'http://video.hrwq.com/',
     'AK'  => 'CNSLO6Zft_HXWPuVve4fvH98kMbfIdlJBJaAlJCA',
